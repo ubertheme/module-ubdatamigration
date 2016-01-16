71594124d3521bb15376c36ef10a43c96933d1b9
Quynh Vu Van <qvv.hn.vn@gmail.com>
2016-01-17 00:18:53 +0700

    Update UpgradeSchema.php

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -5,6 +5,7 @@
 
 namespace Ubertheme\Ubdatamigration\Setup;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
